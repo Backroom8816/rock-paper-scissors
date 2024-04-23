53769c179b9b4c12d7c2eee9327d8ecacd971c2f
Backroom8816 <140431054+Backroom8816@users.noreply.github.com>
2024-04-23 11:37:56 +0200

    Create getComputerChoice function
    This function randomly returns the strings "rock",
    "paper" or "scissors".
    
    This is achieved by creating the variable
    computerChoice.
    The variable uses the Math.random() to create
    a random number between 0 and 0,99.
    Then the generated number is multiplied by 3 to
    increase the random number range (0 to 2,97).
    Finally, Math.floor() rounds down the numbers
    returning the largest integer (0, 1, 2) providing
    3 choices.
    
    The if..else construction tests the value of the
    variable computerChoice and returns the strings
    "rock", "paper" and "scissors" when the variable
    is either 0, 1 or 2 respectively.

--- game.js
+++ game.js
@@ -1,2 +1,38 @@
 // game.js
 console.log("Hello World");
+
+// Does my program have a user interface? No
+// What inputs has the program? One, user input (Rock, Paper, Scissor)
+// What is the desired output? Number of rounds played and the final result
+
+// Get computer choice
+// Get user choice (input)
+// Compare user's choice vs. computer's choice
+// Decide round winner
+// Increase score for round winner
+// Print round winner's score
+// Increase round counter
+// Print number of round
+// While round counter is smaller than 5, continue game
+// Otherwise stop game
+
+// Get computer choice
+    // Math.random() returns a random number between 0 and 0.99
+    // Multiply the generated number by 3 so we expand the range
+    // from 0 to 2,97
+    // then Math.floor rounds down the numbers and returns
+    // the largest integers (0, 1, 2), so we get 3 choices
+
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * 3);
+
+    if (computerChoice == 0) {
+        return "rock";
+    } else if (computerChoice == 1) {
+        return "paper";
+    } else if (computerChoice == 2) {
+        return "scissor";
+    }
+}
+
+console.log(getComputerChoice())
