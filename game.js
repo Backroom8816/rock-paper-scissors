b0148d60d7774c8f47b7a13178b2dc48633d903b
Backroom8816 <140431054+Backroom8816@users.noreply.github.com>
2024-05-31 11:02:28 +0200

    Refactor computer choice
    Rewrite if..else statement within the getComputerChoice() function
    using a switch statement.
    
    The aim of this change is to make the function more readable by
    explicitly showing the mapping of numbers to choices.

--- game.js
+++ game.js
@@ -13,12 +13,13 @@
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 3);
 
-    if (computerChoice == 0) {
-        return "rock";
-    } else if (computerChoice == 1) {
-        return "paper";
-    } else if (computerChoice == 2) {
-        return "scissors";
+    switch (computerChoice) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors"
     }
 }
 
