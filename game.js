ffae18c6af55761e9773f0656181ef96af0134f5
Backroom8816 <140431054+Backroom8816@users.noreply.github.com>
2024-06-03 11:34:58 +0200

    Delete comments

--- game.js
+++ game.js
@@ -1,14 +1,4 @@
 // game.js
-// Does my program have a user interface? No
-// What inputs has the program? One, user input (Rock, Paper, Scissor)
-// What is the desired output? Number of rounds played and the final result
-
-    // Get computer choice
-        // Math.random() returns a random number between 0 and 0.99
-        // Multiply the generated number by 3 the range is expanded
-        // from 0 to 2,97
-        // then Math.floor rounds down the numbers and returns
-        // the largest integers (0, 1, 2), getting 3 choices
 
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 3);
@@ -23,11 +13,6 @@ function getComputerChoice() {
     }
 }
 
-// Get user choice (input)
-    // Ask user to choose between rock, paper or scissor
-    // What if the user inputs an unvalid string?
-        // Make input always lowercase
-
 function getHumanChoice() {
     let humanChoice = prompt("Choose one: rock, paper or scissors", "").toLowerCase();
 
@@ -38,26 +23,15 @@ function getHumanChoice() {
             return humanChoice;
         default:
             alert("Bad input. Try again.")
-            return getHumanChoice(); // Return result of recursive call
+            return getHumanChoice();
     }
 }
 
-// Logic to play the entire game
-    // While the sum of player scores is less than 5, continue game
-    // Otherwise stop game
-
 function playGame() {
     
-    // Declare the player's score variables
     let computerScore = 0;
     let humanScore = 0;
-
-    // Declare round count variable
     let countRound = 0;
-
-    // Logic to play a single round
-        // Compare user's choice vs. computer's choice
-        // Increase score for round winner
 
     function playRound(humanChoice, computerChoice) {
 
